c5b141431ac8f052903e1be3f7c4d6f218b358c2
Hyeonkwon Cho <chkwon91@gmail.com>
2017-08-16 22:55:40 +0900

    Face Detect: Add marginal space on face area
    Add marginal space when cropping face area
    because the minimum size for learning is 224*224
    but most of face images are smaller than the size.
    Appropriate solution will be adding marginal space.
    
    User can set marginal space with third argument(pixel standard)
    
    python face_detect_cv3.py
    [Image directory] [Cropped image store directory] [marginal space]

--- face_detect/face_detect_cv3.py
+++ face_detect/face_detect_cv3.py
@@ -7,16 +7,18 @@
 #
 # python face_detect_cv3.py \
 #        [Image root directory to probe] [Cropped image root directory to store]
+#        [Face marginal space]
 
 import cv2
 import sys
 import os
 import glob
 
-def faceCrop(imagePath):
+def faceCrop(imagePath, faceMargin):
     """ Crop face area in image
     Args:
         imagePath: Image path to detect face area
+        faceMargin: Marginal space on face area
     Returns:
         List of face area.
     """
@@ -36,7 +38,7 @@ def faceCrop(imagePath):
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(224, 224)
+        minSize=(224-faceMargin*2, 224-faceMargin*2)
     )
 
     # if the number of faces exceeds 2, we ignore this image
@@ -45,17 +47,22 @@ def faceCrop(imagePath):
         return cropImageList
 
     for (x, y, w, h) in faces:
-        cropImg = image[y:y+h, x:x+w]
+        # if it cannot be cropped including marginal space, ignore it.
+        if y-faceMargin <= 0 or x-faceMargin <= 0:
+            print("Unable to crop marginal space")
+            return cropImageList
+        cropImg = image[y-faceMargin : y+h+faceMargin, x-faceMargin : x+w+faceMargin]
         cropImageList.append(cropImg) # Note: image format -> [y: y+h, x: x+w]
         #cv2.imshow("cropped", crop_img) # See face area with image viewer.
 
     return cropImageList
 
-def imagesCrop(imageDir, cropDir):
+def imagesCrop(imageDir, cropDir, faceMargin):
     """Probes images and crop/save face area.
     Args:
         imageDir : Directory to probe images.
         cropDir : Directory to save cropped image.
+        faceMargin : Marginal space on face area
     """
     imageList = glob.glob(imageDir + "*.jpg")
 
@@ -66,7 +73,7 @@ def imagesCrop(imageDir, cropDir):
     print("Found {0} images!".format(len(imageList)))
 
     for image in imageList:
-        cropImageList = faceCrop(image)
+        cropImageList = faceCrop(image, faceMargin)
         index = 0
         for cropImage in cropImageList:
             # Cropped image name : cropped_{face index}_{original name}
@@ -74,7 +81,7 @@ def imagesCrop(imageDir, cropDir):
                     + os.path.basename(image), cropImage)
             index += 1
 
-def traverse_folder(imageDir, cropDir):
+def traverse_folder(imageDir, cropDir, faceMargin):
     """Probe folders in image Dir and call imagesCrop
        Image data format is on below.
             -> [root directory]/[image classification]/*.jpg
@@ -83,6 +90,7 @@ def traverse_folder(imageDir, cropDir):
        Arg:
            imageDir : Root directory to probe
            cropDir : Root directory to store cropped images
+           faceMargin : Marginal space on face area
     """
 
     for root, dirs, files in os.walk(imageDir):
@@ -90,11 +98,20 @@ def traverse_folder(imageDir, cropDir):
         for subdir in dirs:
             if not os.path.exists(os.path.join(cropDir, subdir)):
                 os.mkdir(os.path.join(cropDir, subdir))
-            imagesCrop(os.path.join(root, subdir + "/"), os.path.join(cropDir, subdir + "/"))
+            imagesCrop(os.path.join(root, subdir + "/"),
+                    os.path.join(cropDir, subdir + "/"), faceMargin)
 
 def main():
     imageRoot = sys.argv[1]
     cropRoot = sys.argv[2]
-    traverse_folder(imageRoot, cropRoot)
+
+    # Set faceMargin variable if third argument is set.
+    if len(sys.argv) > 3 :
+        print("sys.argv : " + sys.argv[3])
+        faceMargin = int(sys.argv[3])
+    else:
+        faceMargin = 0
+
+    traverse_folder(imageRoot, cropRoot, faceMargin)
 
 main()
