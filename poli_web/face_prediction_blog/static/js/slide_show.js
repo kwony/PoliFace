1ca2d966594a6d57bf2e12dc24af6ab84c749fe3
nomadic90 <nomadic90@gmail.com>
2017-10-11 00:01:31 +0900

    add slide show in home tap but with hard coding

---
+++ poli_web/face_prediction_blog/static/js/slide_show.js
@@ -0,0 +1,45 @@
+function plusSlides(n) {
+  showSlidesClick(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlidesClick(slideIndex = n);
+}
+
+function showSlidesClick(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+
+window.onload = function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  for( i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slideIndex++;
+  if(slideIndex > slides.length) {slideIndex = 1;}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 1000);
+}
