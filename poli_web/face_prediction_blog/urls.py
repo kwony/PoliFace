791019812fc94822b7c442e5c044e18ef82209bd
nomadic90 <nomadic90@gmail.com>
2017-10-24 16:15:41 +0900

    fix korean letter problem

--- poli_web/face_prediction_blog/urls.py
+++ poli_web/face_prediction_blog/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import rank, resemble, info, home
+from .views import rank, resemble, info, info_detail, home
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^home$', home, name='home'),
     url(r'^rank$', rank, name='rank'),
     url(r'^info$', info, name='info'),
+    url(r'^info/(?P<pk>\d+)/$', info_detail, name='info_detail'),
     url(r'^resemble$', resemble, name='resemble'),
 ]
